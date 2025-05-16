db4285b393e843206b773c1b2af13eaa8c0dfa5a
Szymon <szyszkaimyszka@gmail.com>
2025-05-16 16:53:04 +0200

    added form data schema

---
+++ webapp/ProductionOverview/ProductionOverview/Models/FormInput.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductionOverview.Models
+{
+    public class FormInput
+    {
+        public double copper_energy { get; set; }
+        public double copper_production { get; set; }
+        public double iron_energy { get; set; }
+        public double iron_production { get; set; }
+
+    }
+}
