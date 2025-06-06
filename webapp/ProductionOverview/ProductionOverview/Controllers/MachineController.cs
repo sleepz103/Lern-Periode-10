2347fd9d3c2b417acdb9c51f78616cf1e40f4980
Szymon <szyszkaimyszka@gmail.com>
2025-06-06 15:49:09 +0200

    Created new Controller

---
+++ webapp/ProductionOverview/ProductionOverview/Controllers/MachineController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductionOverview.Models;
+using System.Threading.Tasks;
+
+namespace ProductionOverview.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineController : ControllerBase
+    {
+        private readonly ILogger<MachineController> _logger;
+        private readonly InventoryContext _context;
+        public MachineController(ILogger<MachineController> logger, InventoryContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMachines()
+        {
+            var machines = await _context.Machines.ToListAsync();
+            return Ok(machines);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddMachine([FromBody] Machine machine)
+        {
+            _context.Machines.Add(machine);
+            await _context.SaveChangesAsync();
+            if (machine == null)
+            {
+                return BadRequest("Machine cannot be null.");
+            }
+
+            return Ok($"Machine added successfully.");
+        }
+    }
+}
